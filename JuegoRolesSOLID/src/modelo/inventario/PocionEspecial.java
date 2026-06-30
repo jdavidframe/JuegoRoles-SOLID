a9497269c7074f442d22cb1ca5cc4f4f9061578c
Adrian Lopez <israelruiz2874@gmail.com>
2026-06-29 20:26:44 -0500

    Clase Pocion Especial

--- JuegoRolesSOLID/src/modelo/inventario/PocionEspecial.java
+++ JuegoRolesSOLID/src/modelo/inventario/PocionEspecial.java
@@ -6,5 +6,19 @@ import modelo.base.Personaje;
 import modelo.mecanicas.FuerzaAumentada;
 
 public class PocionEspecial extends Objeto {
+     private int curacion;
+    private int bonoAtaqueTemporal;
 
+    public PocionEspecial(String nombre, int curacion, int bonoAtaqueTemporal) {
+        super(nombre);
+        this.curacion = curacion;
+        this.bonoAtaqueTemporal = bonoAtaqueTemporal;
+    }
+
+    public String consumir(Personaje p) {
+        p.setVidaActual(p.getVidaActual() + curacion);
+        p.agregarEstado(new FuerzaAumentada(2, bonoAtaqueTemporal));
+        return p.getNombre() + " consumió " + nombre + ". Recuperó " + curacion + " HP y su ataque aumentó temporalmente.";
+    }
 }
+
