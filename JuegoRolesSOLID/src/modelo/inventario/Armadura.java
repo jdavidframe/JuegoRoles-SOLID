870f640452ed054be0c7722616bd69ff20b35026
Adrian Lopez <israelruiz2874@gmail.com>
2026-06-29 20:25:54 -0500

    Clase Armadura

--- JuegoRolesSOLID/src/modelo/inventario/Armadura.java
+++ JuegoRolesSOLID/src/modelo/inventario/Armadura.java
@@ -4,7 +4,17 @@ package modelo.inventario;
 import modelo.base.Objeto;
 import modelo.base.Personaje;
 
-
 public class Armadura extends Objeto {
+    private int bonificadorDefensa;
 
+    public Armadura(String nombre, int bonificadorDefensa) {
+        super(nombre);
+        this.bonificadorDefensa = bonificadorDefensa;
+    }
+
+    @Override
+    public int getBonificadorDefensa() { 
+        return bonificadorDefensa; 
+    }
 }
+
