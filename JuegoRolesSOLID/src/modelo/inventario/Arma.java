b2002b5c6475d2dc8052a9926c4f61329e37fb60
Adrian Lopez <israelruiz2874@gmail.com>
2026-06-29 20:23:52 -0500

    Clase Arma

--- JuegoRolesSOLID/src/modelo/inventario/Arma.java
+++ JuegoRolesSOLID/src/modelo/inventario/Arma.java
@@ -1,16 +1,20 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package modelo.inventario;
 
 import modelo.base.Objeto;
 import modelo.base.Personaje;
 
-/**
- *
- * @author jesudavi
- */
 public class Arma extends Objeto {
+    private int bonificadorAtaque;
 
+    public Arma(String nombre, int bonificadorAtaque) {
+        super(nombre);
+        this.bonificadorAtaque = bonificadorAtaque;
+    }
+
+    @Override
+    public int getBonificadorAtaque() { 
+        return bonificadorAtaque; 
+    }
 }
+
--- JuegoRolesSOLID/src/modelo/inventario/Armadura.java
+++ JuegoRolesSOLID/src/modelo/inventario/Armadura.java
@@ -1,16 +1,10 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package modelo.inventario;
 
 import modelo.base.Objeto;
 import modelo.base.Personaje;
 
-/**
- *
- * @author jesudavi
- */
+
 public class Armadura extends Objeto {
 
 }
