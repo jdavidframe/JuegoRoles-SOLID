05c4d279e62a53044e877ef8157c8f3f551f031d
juampyz7 <jlojaquichimbo@gmail.com>
2026-06-29 21:06:25 -0500

    Rama Personajes 3

--- JuegoRolesSOLID/src/modelo/personajes/Mago.java
+++ JuegoRolesSOLID/src/modelo/personajes/Mago.java
@@ -1,16 +1,18 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package modelo.personajes;
 
 import modelo.base.Personaje;
 import modelo.interfaces.IHabilidad;
 
-/**
- *
- * @author jesudavi
- */
 public class Mago extends Personaje {
+    private int intelecto;
 
+    public Mago(String nombre, int vidaMax, int ataqueBase, int defensaBase, int intelecto, IHabilidad habilidad) {
+        super(nombre, vidaMax, ataqueBase, defensaBase, 100, habilidad);
+        this.intelecto = intelecto;
+    }
+
+    @Override
+    public int calcularAtaque() {
+        return super.calcularAtaque() + this.intelecto;
+    }
 }
